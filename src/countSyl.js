3a218d386b5d3a05a4354a9f6ca601c856d99f5f
Adrian Messado and Matt Nardoni <adrian@>
2018-04-19 11:25:51 -0700

    add import for WordTest, add export of class

---
+++ src/countSyl.js
@@ -0,0 +1,13 @@
+import { WordTest } from './../src/scripts.js';
+
+class CountSyl {
+  constructor(count) {
+    this.numbOfSyl = numbOfSyl;
+  }
+    letterCount() {
+    WordTest.result.length();
+    }
+}
+
+
+export {CountSyl};
